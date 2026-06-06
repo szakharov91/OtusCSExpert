a0891949d080013a39688c46742f3061ff513853
Zakharov Svyatoslav <kraldraav@gmail.com>
2026-06-06 15:30:16 +0300

    HW#3 Point1: Add ReaderWriterLockSlim to Storage class

--- src/OtusCSExpert.Common/Storage/SimpleStore.cs
+++ src/OtusCSExpert.Common/Storage/SimpleStore.cs
@@ -9,33 +9,49 @@ namespace OtusCSExpert.Common.Storage;
 /// <summary> Реализация базового хранилища </summary>
 public class SimpleStore : IStoragable
 {
-    private readonly Dictionary<string, byte[]> _keyValuePairs;
-
-    public SimpleStore()
-    {
-        _keyValuePairs = new Dictionary<string, byte[]>();
-    }
+    private readonly Dictionary<string, byte[]> _keyValuePairs = [];
+    private readonly ReaderWriterLockSlim _lock = new ReaderWriterLockSlim();
 
     public void Delete(string key)
     {
         ArgumentNullException.ThrowIfNull(key, nameof(key));
-        _keyValuePairs.Remove(key);
+        _lock.EnterWriteLock();
+        try
+        {
+            _keyValuePairs.Remove(key);    
+        }
+        finally
+        {
+            _lock.ExitWriteLock();
+        }
     }
 
     public byte[] Get(string key)
     {
         ArgumentNullException.ThrowIfNull(key, nameof(key));
-
-        if (!_keyValuePairs.TryGetValue(key, out byte[]? value))
-            return null;
-
-        return value;
+        _lock.EnterReadLock();
+        try
+        {
+            return !_keyValuePairs.TryGetValue(key, out byte[]? value) ? null : value;
+        }
+        finally
+        {
+            _lock.ExitReadLock();
+        }
     }
 
     public void Set(string key, byte[] value)
     {
         ArgumentNullException.ThrowIfNull(key, nameof(key));
         ArgumentNullException.ThrowIfNull(value, nameof(value));
-        _keyValuePairs[key] = value;
+        _lock.EnterWriteLock();
+        try
+        {
+            _keyValuePairs[key] = value;
+        }
+        finally
+        {
+            _lock.ExitWriteLock();
+        }
     }
 }
